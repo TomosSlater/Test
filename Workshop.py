f3f9c4ea176f0a1e7e28c3acacba9d0d0c6bc423
Tomos Slater <Tomos.Slater@softwire.com>
2021-10-12 13:54:28 +0100

    add power operator

--- Workshop.py
+++ Workshop.py
@@ -7,6 +7,8 @@ def calculate(operator, num_1, num_2):
         return int(num_1) * int(num_2)
     if operator ==  '/':
         return float(num_1) / float(num_2)
+    if operator == '^':
+        return int(num_1) ^ int(num_2)
 
 def calculate_and_print(self, operator, num_1, num_2):
     print(self.calculate(operator, num_1, num_2))
